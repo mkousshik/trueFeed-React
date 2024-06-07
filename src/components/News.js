0c6e55bdabc75af21ba99552dc84a12263867fbd
Koushik Mandal <koushikmandal05@gmail.com>
2024-06-07 15:16:55 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class  extends Component {
+  render() {
+    return (
+      <div>
+          <NewsItem/>
+      </div>
+    )
+  }
+}
