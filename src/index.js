0606704c3b72aeef4ded7d2eb7fcd1fcfbf01596
koushikmandal05 <koushikmandal05@gmail.com>
2024-06-13 06:42:52 +0000

    google idx testing

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
   /* </React.StrictMode> */
 
 );
-
+// koushik - update form google idx , idx testing
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
